77b77233079ac2fa7a7c2345c6bf6d9ff9a5d4be
Jacob Franz <franzj@fb.com>
2020-07-06 17:52:46 -0500

    Added sign up functionality

---
+++ franz-instagram/View Controllers/FeedViewController.h
@@ -0,0 +1,17 @@
+//
+//  FeedViewController.h
+//  franz-instagram
+//
+//  Created by Jacob Franz on 7/6/20.
+//  Copyright © 2020 Jacob Franz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FeedViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ franz-instagram/View Controllers/LoginViewController.m
@@ -0,0 +1,65 @@
+//
+//  LoginViewController.m
+//  franz-instagram
+//
+//  Created by Jacob Franz on 7/6/20.
+//  Copyright © 2020 Jacob Franz. All rights reserved.
+//
+
+#import "LoginViewController.h"
+#import <Parse/Parse.h>
+
+@interface LoginViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *usernameField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordField;
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (IBAction)didTapSignup:(id)sender {
+    PFUser *newUser = [PFUser user];
+    
+    newUser.username = self.usernameField.text;
+    newUser.password = self.passwordField.text;
+    
+    [newUser signUpInBackgroundWithBlock:^(BOOL succeeded, NSError * _Nullable error) {
+        if (error != nil) {
+            NSLog(@"Error signing up: %@", error.localizedDescription);
+            [self sendLoginAlert];
+        } else {
+            NSLog(@"Successfully registered user!");
+            [self performSegueWithIdentifier:@"loginSegue" sender:nil];
+        }
+    }];
+}
+
+- (IBAction)didTapLogin:(id)sender {
+}
+
+- (void)sendLoginAlert {
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Account Error" message:@"An error occurred with your account. Please check that you entered a valid username and password, then try again." preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *dismissAction = [UIAlertAction actionWithTitle:@"Dismiss" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+    }];
+    [alert addAction:dismissAction];
+    [self presentViewController:alert animated:YES completion:^{
+    }];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
