c0632e4743fb7e6d33a67a2754901180df3dc69a
Jacob Franz <franzj@fb.com>
2020-07-06 17:02:39 -0500

    Initial Commit

---
+++ franz-instagram/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  franz-instagram
+//
+//  Created by Jacob Franz on 7/6/20.
+//  Copyright © 2020 Jacob Franz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ franz-instagram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  franz-instagram
+//
+//  Created by Jacob Franz on 7/6/20.
+//  Copyright © 2020 Jacob Franz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
